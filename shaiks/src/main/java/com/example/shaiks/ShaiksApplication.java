55e7b60c4221b124b5e98aceaf84e4b014c6b174
smdimran2000 <shaikmdimran31@gmail.com>
2023-02-14 15:57:00 +0530

    git-code

---
+++ shaiks/src/main/java/com/example/shaiks/ShaiksApplication.java
@@ -0,0 +1,13 @@
+package com.example.shaiks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShaiksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShaiksApplication.class, args);
+	}
+
+}
